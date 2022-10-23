1c9adb7cac123fac42b7ce305beb67b36dd945c7
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-10-23 20:34:01 +0530

    Delete exercise_6 directory

--- exercise_6/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-import time
-s = socket.socket()
-s.connect(("localhost", 1450))
-while 1:
-    msg = s.recv(2).decode()
-    print("Received --> ", int(msg))
-    s.send(str(msg).encode())
-    time.sleep(1)
